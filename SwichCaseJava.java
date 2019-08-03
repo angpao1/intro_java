283b4c2df0102e8fcb5960f7afa7c97b19df4d32
angpao <angpao.kittimasak@gmail.com>
2019-08-03 15:25:29 +0700

    SwichCase

---
+++ SwichCaseJava.java
@@ -0,0 +1,17 @@
+public class SwichCaseJava {
+    public static void main(String args[]) {
+        int num = 2;
+        switch (num) {
+        case 1:
+            System.out.println("Case1: Value is: " + num);
+        case 2:
+            System.out.println("Case2: Value is: " + num);
+            break;
+        case 3:
+            System.out.println("Case3: Value is: " + num);
+            break;
+        default:
+            System.out.println("Default: Value is: " + num);
+        }
+    }
+}
